e0e3cf0334e6ecc0fbc95112484eaabf9677380d
Farshad Davoudi <f.davoudi.r@outlook.com>
2024-01-06 01:30:38 +0330

    Add winget + Add Dapr configs #20

---
+++ winget-apps.ps1
@@ -0,0 +1,2 @@
+winget install Dapr.CLI # Install the latest Windows Dapr CLI to $Env:SystemDrive\dapr and add this directory to the user PATH environment variable
+winget install XP8K1GHCB0F1R2 # RedisInsight
