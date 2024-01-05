5b04bbf2115e037fcad3b68435331df3515bceac
Farshad Davoudi <f.davoudi.r@outlook.com>
2024-01-06 01:56:14 +0330

    Add Dapr projects #21

---
+++ dapr-setup/DaprProjB/Controllers/EventSubscriberController.cs
@@ -0,0 +1,38 @@
+﻿using Dapr;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DaprProjB.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class EventSubscriberController : ControllerBase
+{
+    [Topic("rabbitmq-pubsub", "test-topic", match: "event.type == \"route1\"", 1)]
+    [HttpPost]
+    public async Task<IActionResult> TestEvent1(Person person)
+    {
+        Console.WriteLine("Subscriber received - RouteKey1 : " + person.FullName);
+        return Ok();
+    }
+
+    [Topic("rabbitmq-pubsub", "test-topic", match: "event.type == \"route2\"", 2)]
+    [HttpPost]
+    public async Task<IActionResult> TestEvent2(Person person)
+    {
+        Console.WriteLine("Subscriber received - RouteKey2 : " + person.FullName);
+        return Ok();
+    }
+
+    [Topic("rabbitmq-pubsub", "test-topic")]
+    [HttpPost]
+    public async Task<IActionResult> TestEventDefault(Person person)
+    {
+        Console.WriteLine("Subscriber received - RouteKey3 : " + person.FullName);
+        return Ok();
+    }
+
+    public class Person
+    {
+        public string? FullName { get; set; }
+    }
+}
