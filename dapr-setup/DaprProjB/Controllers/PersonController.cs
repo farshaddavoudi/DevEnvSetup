d79e2834349f08f19605acbdc8dc5d10a71c22c6
Farshad Davoudi <f.davoudi.r@outlook.com>
2024-04-11 21:03:42 +0330

    Update Dapr configs and change name resolution component to SQLite #32

--- dapr-setup/DaprProjB/Controllers/PersonController.cs
+++ dapr-setup/DaprProjB/Controllers/PersonController.cs
@@ -1,12 +1,14 @@
 using DaprProjB.ActionFilters;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DaprProjB.Controllers;
 
 [ApiController]
 [Route("[controller]/[action]")]
-public class PersonController(ILogger<WeatherForecastController> logger) : ControllerBase
+public class PersonController() : ControllerBase
 {
+    [AllowAnonymous]
     [DaprAuthorize]
     [HttpGet]
     public IActionResult Friend()
